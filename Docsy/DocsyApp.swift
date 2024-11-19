c14cf4a2ae0dcce4d41b9d8e2f116ea07937336c
Noah Kamara <mail@noahkamara.com>
2024-11-19 22:41:27 +0100

    Initial Commit

---
+++ Docsy/DocsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocsyApp.swift
+//  Docsy
+//
+//  Created by Noah Kamara on 19.11.24.
+//
+
+import SwiftUI
+
+@main
+struct DocsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocsyTests/DocsyTests.swift
@@ -0,0 +1,16 @@
+//
+//  DocsyTests.swift
+//  DocsyTests
+//
+//  Created by Noah Kamara on 19.11.24.
+//
+
+import Testing
+
+struct DocsyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
